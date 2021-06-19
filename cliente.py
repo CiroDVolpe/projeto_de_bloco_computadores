0589eb2bda9e9496dd1567a680ffe0a1222be119
Ciro Della Volpe <cirodvolpe@gmail.com>
2021-06-19 00:11:32 -0300

    TP9 feito

--- cliente.py
+++ cliente.py
@@ -9,12 +9,9 @@ sg.theme("LightBlue")
 HOST = socket.gethostname()
 PORT = 8888
 DEST = (HOST, PORT)
+
 RIGHT = '1111011100000011'
 LEFT = '1111011100000010'
-DATE_FORMAT = "%d/%m/%Y às %T"
-TIME_FORMAT = '%H:%M:%S'
-NUM_OF_PROCESS = 5
-MAX_HOST_NUM = 10
 
 ######################################################################################################
 
@@ -27,7 +24,7 @@ def send_to_server(topic):
   tcp.send(topic.encode('ascii'))
 
 def get_from_server():
-  received = tcp.recv(1024)
+  received = tcp.recv(8192)
   loaded = pickle.loads(received)
   print(loaded)
   return loaded
@@ -40,6 +37,53 @@ def load_dict(info):
 # INITIAL
 send_to_server('initial')
 initial_info = load_dict(get_from_server())
+
+initial_cpu_info = initial_info["cpu"]
+initial_files_info = initial_info["files"]
+initial_process_info = initial_info["process"]
+initial_ip_info = initial_info["ip"]
+initial_net_info = initial_info["net"]
+
+######################################################################################################
+
+# CPU
+cpu_layout = [ [sg.Text("Informações do Processador")],
+  [
+    sg.Text(f"Nome e modelo de CPU: {initial_cpu_info['name']}")
+  ],
+  [
+    sg.Text(f"Tipo de arquitetura: {initial_cpu_info['arch']}") 
+  ],
+  [
+    sg.Text(f"Palavra do processador (bits): {initial_cpu_info['bits']}")
+  ],
+  [
+    sg.Text(f"Frequencia min e max da CPU (MHz): {initial_cpu_info['freq_min']} || {initial_cpu_info['freq_max']}")
+  ],
+  [
+    sg.Text(f"Frequencia atual da CPU (MHz): {initial_cpu_info['freq_cur']}")
+  ],
+  [
+    sg.Text(f"Numero total de núcleos físicos: {initial_cpu_info['fis_cores']}")
+  ],
+  [
+    sg.Text(f"Numero total de núcleos lógicos: {initial_cpu_info['log_cores']}")
+  ]
+]+ \
+[
+  [
+    sg.Text(f"Uso de CPU {x+1} (%)"),
+    sg.ProgressBar(100, orientation='h', size=(20, 20), key=f'cpu_percentage-{x}')
+  ] for x in range(initial_cpu_info["num_of_cpus"])
+]
+
+def att_cpu():
+  send_to_server('cpu')
+  cpus = load_dict(get_from_server())
+  for x in range(initial_cpu_info["num_of_cpus"]):
+    cpu_bar = window[f'cpu_percentage-{x}']
+    per_cpu = cpus[x]
+    cpu_bar.UpdateBar(per_cpu)
 
 ######################################################################################################
 
@@ -77,8 +121,197 @@ def att_disk():
 
 ######################################################################################################
 
+# FILES
+files_layout = [ [sg.Text("Informações de Arquivos e Diretórios")],
+  [
+    sg.Text(f"Nome do diretório atual: {initial_files_info['dir']}")
+  ],
+  [sg.Text("Dados do escalonamento de 1 segundo com sleep de 1 segundo da função de verificação de arquivos: ")],
+  [sg.Text("Tempo inicial: CLIQUE EM RODAR PARA RECEBER UM VALOR", key="start_time_text")],
+  [sg.Text("Tempo chamada: CLIQUE EM RODAR PARA RECEBER UM VALOR", key="call_time_text")],
+  [sg.Text("Tempo final da execução: CLIQUE EM RODAR PARA RECEBER UM VALOR", key="exec_time_text")],
+  [sg.Text("Duração da chamada: CLIQUE EM RODAR PARA RECEBER UM VALOR", key="proc_time_text")],
+  [sg.Text("Quantidade total de clocks para a realização do inicio até o final: CLIQUE EM RODAR PARA RECEBER UM VALOR", key="clock_time_text")],
+  [sg.Button('RODAR')]
+]+ \
+[
+  [
+    sg.Text(f"Nome do arquivo: {initial_files_info['files_specs'][x]['name']}"),
+    sg.Text(f"Tamanho: {initial_files_info['files_specs'][x]['size']} bytes"),
+    sg.Text(f"Data de criação: {initial_files_info['files_specs'][x]['creation_date']}"),
+    sg.Text(f"Data de modificação: {initial_files_info['files_specs'][x]['modified_date']}"),
+    sg.Text(f"Tipo: {initial_files_info['files_specs'][x]['type']}")
+  ] for x in range(initial_files_info['num_of_files'])
+]
+
+def att_schedule():
+  send_to_server('schedule')
+
+  start_time_field = window['start_time_text']
+  call_time_field = window['call_time_text']
+  exec_time_field = window['exec_time_text']
+  proc_time_field = window['proc_time_text']
+  clock_time_field = window['clock_time_text']
+
+  schedule_info = load_dict(get_from_server())
+
+  start_time = schedule_info['start_time']
+  call_time = schedule_info['call_time']
+  exec_time = schedule_info['exec_time']
+  exec_diff_time = schedule_info['exec_diff_time']
+  clock_diff_time = schedule_info['clock_diff_time']
+
+  start_time_field.Update(f"Tempo inicial: {start_time}")
+  call_time_field.Update(f"Tempo chamada (c/ delay): {call_time}")
+  exec_time_field.Update(f"Tempo final da execução (c/ sleep): {exec_time}")
+  proc_time_field.Update(f"Duração da chamada (c/ sleep): {exec_diff_time}")
+  clock_time_field.Update(f"Quantidade total de clocks para a realização do inicio até o final: {clock_diff_time} segundo")
+
+######################################################################################################
+
+# PROCESS
+process_layout = [ [sg.Text("Informações de Processos (Servidor)")],
+  [
+    sg.Text(f"PID do processo do Python: {initial_process_info['pid']}"),
+    sg.Text(f"Porcentagem do processador no processo do programa: {initial_process_info['perc']}", key="python_cpu_percent")
+  ]
+]+ \
+[
+  [
+    sg.Text(f"PID: CARREGANDO LISTA, POR FAVOR ESPERE", key=f"process_pid-{x}"),
+    sg.Text(f"Nome do executável: CARREGANDO LISTA, POR FAVOR ESPERE", key=f"process_name-{x}"),
+    sg.Text(f"Consumo de processamento: CARREGANDO LISTA, POR FAVOR ESPERE", key=f"process_cpu_percent-{x}"),
+    sg.Text(f"Consumo de memória: CARREGANDO LISTA, POR FAVOR ESPERE", key=f"process_vms-{x}"),
+    sg.Text(f"Usuário: CARREGANDO LISTA, POR FAVOR ESPERE", key=f"process_username-{x}")
+  ] for x in range(initial_process_info['num_of_processes'])
+]
+
+def att_process():
+  send_to_server('process')
+  processes = load_dict(get_from_server())
+
+  python_cpu_text = window["python_cpu_percent"]
+  python_cpu_percent = processes["my_cpu_percent"]
+  python_cpu_text.Update(f"Porcentagem do processador no processo do programa: {python_cpu_percent}")
+
+  for x in range(initial_process_info["num_of_processes"]):
+    process_pid = window[f"process_pid-{x}"]
+    process_name = window[f"process_name-{x}"]
+    process_cpu_percent = window[f"process_cpu_percent-{x}"]
+    process_vms = window[f"process_vms-{x}"]
+    process_username = window[f"process_username-{x}"]
+
+    pid = processes['processes'][x]['pid']
+    name = processes['processes'][x]['name']
+    cpu_percent = processes['processes'][x]['cpu_percent']
+    memory_usage = processes['processes'][x]['memory_usage']
+    username = processes['processes'][x]['username']
+
+    process_pid.Update(f"PID: {pid}")
+    process_name.Update(f"Nome do executável: {name}")
+    process_cpu_percent.Update(f"Consumo de processamento: {cpu_percent} (%)")
+    process_vms.Update(f"Consumo de memória: {memory_usage}")
+    process_username.Update(f"Usuário: {username}")
+
+######################################################################################################
+
+# NET
+interfaces = initial_net_info["interfaces"]
+usage_per_interface = initial_net_info["usage_per_interface"]
+usage_per_process = initial_net_info["usage_per_process"]
+
+net_layout = [ [sg.Text("Informações das Conexões em Rede")],
+  [
+    sg.Text("Interfaces conectadas"),
+    sg.Multiline(f'{interfaces}', size=(100, 20), disabled=True)
+  ],
+  [
+    sg.Text("Uso de dados de rede por interface"),
+    sg.Multiline(f'{usage_per_interface}', size=(100, 20), key=f"net_interfaces_ios", disabled=True)
+  ],
+  [
+    sg.Text("Uso de dados de rede por processo"),
+    sg.Multiline(f'{usage_per_process}', size=(100, 20), key=f"net_processes", disabled=True)
+  ]
+]
+
+def att_net():
+  send_to_server('net')
+
+  net_interfaces_ios = window['net_interfaces_ios']
+  net_processes = window['net_processes']
+
+  summary_info = load_dict(get_from_server())
+
+  usage_per_interface = summary_info["usage_per_interface"]
+  usage_per_process = summary_info["usage_per_process"]
+
+  net_interfaces_ios.Update(usage_per_interface)
+  net_processes.Update(usage_per_process)
+
+######################################################################################################
+
+# IP
+ip_layout = [ [sg.Text("Informações da SubRede")],
+  [sg.Text(f"IP da máquina atual: {initial_ip_info['my_ip']}")],
+  [
+    sg.Text("Digite um IP"),
+    sg.InputText(key='EntradaIP'),
+    sg.Button('BUSCAR')
+  ],
+  [
+    sg.Text("Esse processo pode demorar!"),
+  ]
+]+ \
+[
+  [
+    sg.Text(f"HOST {x + 1}", key=f"subnet_host-{x}", visible=False),
+    sg.Text("IP: 000.000.000.000 Placeholder (Nome Placeholder Nome Placeholder Nome Placeholder Nome Placeholder)", key=f"subnet_host_ip-{x}", visible=False),
+    sg.Text("Estado: Estado Placeholder", key=f"subnet_host_state-{x}", visible=False),
+    sg.Text("Protocolos disponiveis: Protocolos Placeholder", key=f"subnet_host_protocols-{x}", visible=False),
+    sg.Text("Portas disponíveis e suas infos:", key=f"subnet_host_ports_text-{x}", visible=False),
+    sg.Multiline("Portas disponíveis e suas infos: Portas Placeholder Estado da Porta: Estado Placeholder", key=f"subnet_host_ports-{x}",disabled=True, visible=False)
+  ] for x in range(initial_ip_info["max_num_hosts"])
+]
+
+def att_subnet(args):
+  send_to_server(f'subnet?{args}')
+
+  subnet_info = load_dict(get_from_server())
+
+  for x in range(subnet_info["len"]):
+    subnet_host = window[f"subnet_host-{x}"]
+    subnet_host_ip = window[f"subnet_host_ip-{x}"]
+    subnet_host_state = window[f"subnet_host_state-{x}"]
+    subnet_host_protocols = window[f"subnet_host_protocols-{x}"]
+    subnet_host_ports_text = window[f"subnet_host_ports_text-{x}"]
+    subnet_host_ports = window[f"subnet_host_ports-{x}"]
+
+    ip = subnet_info['hosts'][x]['ip']
+    state = subnet_info['hosts'][x]['state']
+    protocols = subnet_info['hosts'][x]['protocols']
+    ports = subnet_info['hosts'][x]['ports']
+
+    subnet_host.Update(f"HOST {x + 1}")
+    subnet_host.update(visible=True)
+
+    subnet_host_ip.Update(f"IP: {ip}")
+    subnet_host_ip.update(visible=True)
+
+    subnet_host_state.Update(f"Estado:: {state}")
+    subnet_host_state.update(visible=True)
+
+    subnet_host_protocols.Update(f"Protocolos disponiveis: {protocols}")
+    subnet_host_protocols.update(visible=True)
+
+    subnet_host_ports.Update(f"{ports}")
+    subnet_host_ports_text.update(visible=True)
+    subnet_host_ports.update(visible=True)
+
+######################################################################################################
+
 # SUMMARY
-summary_layout = [ [sg.Text("Resumo")],
+summary_layout = [[sg.Text("Resumo")],
   [
     sg.Text(f"Uso de CPU (%)"),
     sg.ProgressBar(100, orientation='h', size=(20, 20), key='summary_cpu_percentage')
@@ -116,15 +349,25 @@ def att_summary():
 
 # LAYOUT
 layout_carousel = deque([
+  ['cpu', cpu_layout],
   ['memory', memory_layout],
-  ['disk', disk_layout]
+  ['disk', disk_layout],
+  ['files', files_layout],
+  ['process', process_layout],
+  ['net', net_layout],
+  ['ip', ip_layout]
 ])
 
 layout = [
   [
-    sg.Column(memory_layout, key='memory'),
-    sg.Column(disk_layout, visible=False, key='disk'),
-    sg.Column(summary_layout, visible=False, key='summary'),
+    sg.Column(cpu_layout, key='cpu'),
+    sg.Column(memory_layout, key='memory', visible=False),
+    sg.Column(disk_layout, key='disk', visible=False),
+    sg.Column(files_layout, key='files', visible=False),
+    sg.Column(process_layout, key='process', visible=False),
+    sg.Column(net_layout, key='net', visible=False),
+    sg.Column(ip_layout, key='ip', visible=False),
+    sg.Column(summary_layout, key='summary', visible=False),
   ],
   [sg.Button('<'), sg.Text('Aperte ESPAÇO para ver ou sair do resumo.'), sg.Button('>'), sg.Cancel('Sair')]
 ]
@@ -143,17 +386,27 @@ def event_is_direction(event, direction):
 
 # ATT
 def info_att(topic, args = ''):
+  if topic == 'cpu':
+    att_cpu()
   if topic == 'memory':
     att_memory()
   if topic == 'disk':
     att_disk()
+  if topic == 'files':
+    pass
+  if topic == 'process':
+    att_process()
+  if topic == 'net':
+    att_net()
+  if topic == 'ip':
+    pass
   if topic == 'summary':
     att_summary()
 
 ######################################################################################################
 
 # MAIN
-window = sg.Window('TP8 - CLIENTE - Dados sobre o PC', layout, return_keyboard_events=True, use_default_focus=False)
+window = sg.Window('TP9 - CLIENTE - Dados sobre o PC', layout, return_keyboard_events=True, use_default_focus=False)
 
 while True:
   event, values = window.read(timeout=1000)
@@ -182,11 +435,11 @@ while True:
         window[current_topic()].update(visible=False)
         layout_carousel.rotate(-1)
         window[current_topic()].update(visible=True)
-      # if event == 'RODAR':
-      #   schedule_finder()
-      # if event == 'BUSCAR':
-      #   args = values['EntradaIP']
-      #   info_att(current_topic(), args)
+      if event == 'RODAR':
+        att_schedule()
+      if event == 'BUSCAR':
+        args = values['EntradaIP']
+        att_subnet(args)
   
   info_att('summary' if window['summary'].visible else current_topic())
 
--- servidor.py
+++ servidor.py
@@ -13,33 +13,226 @@ from datetime import datetime
 HOST = socket.gethostname()
 PORT = 8888
 ORIG = (HOST, PORT)
-CPU_INFO = cpuinfo.get_cpu_info()
-NUM_OF_CPUS = len(psutil.cpu_percent(interval=1, percpu=True))
-DIR = './files'
-FILES = os.listdir(DIR)
-NM = PortScanner()
-start_time = call_time = exec_time = start_clock = exec_clock = 0
 
+DIR = './files'
+NUM_OF_PROCESS = 5
+MAX_HOST_NUM = 10
+MY_PID = os.getpid()
+
+######################################################################################################
+
+# BASE
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp.bind(ORIG)
+
+def format_date(date):
+  return datetime.fromtimestamp(date).strftime("%d/%m/%Y às %T")
+
+def format_file_path(file):
+  return(DIR + '/' + file)
 
 def encoded_resp(resp, client):
   byte_resp = pickle.dumps(str(resp))
   client.send(byte_resp)
 
+######################################################################################################
+
+# PROCESS
+def cpu_percent(pid):
+  test_list = []
+  for i in range(5):
+    p = psutil.Process(pid)
+    p_cpu = p.cpu_percent(interval=0.1)
+    test_list.append(p_cpu)
+  return round(float(sum(test_list))/len(test_list), 2)
+
+def processes_info_getter():
+  processes = get_process_list()
+
+  processes_info = []
+  for p in processes:
+    processes_info.append(fill_process_info(p))
+  
+  return processes_info
+
+def get_process_list():
+  listOfProcObjects = []
+
+  for proc in psutil.process_iter():
+    try:
+      pinfo = proc.as_dict(attrs=['pid', 'name', 'username', 'cpu_percent'])
+      pinfo['memory_usage'] = round(proc.memory_info().vms / (1024 * 1024), 2)
+
+      listOfProcObjects.append(pinfo)
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+      pass
+
+  listOfProcObjects = sorted(listOfProcObjects, key=lambda process: process['cpu_percent'], reverse=True)
+
+  return listOfProcObjects[:NUM_OF_PROCESS]
+
+def fill_process_info(process):
+  process_info = {
+    "pid": process['pid'],
+    "name": process['name'],
+    "cpu_percent": process['cpu_percent'],
+    "memory_usage": process['memory_usage'],
+    "username": process['username']
+  }
+  return process_info
+
+######################################################################################################
+
+# NET
+def obtem_nome_familia(familia):
+  if familia == socket.AF_INET:
+    return("IPv4")
+  elif familia == socket.AF_INET6:
+    return("IPv6")
+  elif familia == socket.AF_UNIX:
+    return("Unix")
+  else:
+    return("-")
+
+def obtem_tipo_socket(tipo):
+  if tipo == socket.SOCK_STREAM:
+    return("TCP")
+  elif tipo == socket.SOCK_DGRAM:
+    return("UDP")
+  elif tipo == socket.SOCK_RAW:
+    return("IP")
+  else:
+    return("-")
+
+def interfaces():
+  interfaces = psutil.net_if_addrs()
+  interfaces_text = ""
+
+  for i in interfaces:
+    nome = str(i)
+    interfaces_text = interfaces_text + f"{nome}: \n"
+    for info in interfaces[nome]:
+      interfaces_text = interfaces_text + f"\t Endereço: {info.address} | Mascara de rede: {info.netmask} | Familia: {obtem_nome_familia(info.family)} \n"
+  return interfaces_text
+
+def usage_per_interface():
+  io_status = psutil.net_io_counters(pernic=True)
+  ios_text = ""
+  for i in io_status:
+    ios_text = ios_text + f"{str(i)} \n"
+    info = io_status[str(i)]
+    ios_text = ios_text + f"\t BYTES: Enviados: {info.bytes_sent} | Recebidos: {info.bytes_recv} \n\t PACOTES: Enviados: {info.packets_sent} | Recebidos: {info.packets_recv} \n"
+  return ios_text
+
+def usage_per_process():
+  n = 0
+  pids_text = ""
+  pids = psutil.pids()
+  for pid in pids:
+    try:
+      p = psutil.Process(pid)
+      conns = p.connections()
+      if (conns):
+        if n >= NUM_OF_PROCESS:
+          break
+        n += 1
+        for conn in conns:
+          familia = obtem_nome_familia(conn.family)
+          tipo = obtem_tipo_socket(conn.type)
+          status = conn.status
+          ip_porta_local = "<" + conn.laddr[0] + ":" + str(conn.laddr[1]) + ">"
+          if (conn.raddr):
+            ip_porta_remote = "<" + conn.raddr[0] + ":" + str(conn.raddr[1]) + ">"
+          else:
+            ip_porta_remote = "<>"
+          pids_text = pids_text + f"PID: {pid}\n \tFamilia: {familia} | Tipo: {tipo} | status: {status} | IP local: {ip_porta_local} | IP remote: {ip_porta_remote}\n\n"
+    except:
+      pass
+  return pids_text
+
+######################################################################################################
+
+# INITIAL
 def initial_spec():
+  CPU_INFO = cpuinfo.get_cpu_info()
+
+  FILES = os.listdir(DIR)
+  files_specs =[]
+  for arq in FILES:
+    files_specs.append({
+      "name": arq,
+      "size": os.path.getsize(format_file_path(arq)),
+      "creation_date": format_date(os.stat(format_file_path(arq)).st_ctime),
+      "modified_date": format_date(os.stat(format_file_path(arq)).st_mtime),
+      "type": arq.split('.')[1]
+    })
+
+  interfaces_text = interfaces()
+  usage_per_interface_text = usage_per_interface()
+  usage_per_process_text = usage_per_process()
+
   initial_info = {
+    "cpu": {
+      "num_of_cpus": len(psutil.cpu_percent(interval=1, percpu=True)),
+      "name": platform.platform(),
+      "arch": CPU_INFO["arch"],
+      "bits": CPU_INFO["bits"],
+      "freq_min": psutil.cpu_freq().min,
+      "freq_max": psutil.cpu_freq().max,
+      "freq_cur": psutil.cpu_freq().current,
+      "fis_cores": psutil.cpu_count(logical=False),
+      "log_cores": psutil.cpu_count()
+    },
+    "files": {
+      "dir": os.getcwd(),
+      "num_of_files": len(FILES),
+      "files_specs": files_specs
+    },
+    "process": {
+      "pid": MY_PID,
+      "perc": cpu_percent(MY_PID),
+      "num_of_processes": NUM_OF_PROCESS
+    },
+    "net": {
+      "interfaces": interfaces_text,
+      "usage_per_interface": usage_per_interface_text,
+      "usage_per_process": usage_per_process_text
+    },
+    "ip": {
+      "my_ip": socket.gethostbyname(socket.gethostname()),
+      "max_num_hosts": MAX_HOST_NUM
+    },
     "summary": {
       "ip": socket.gethostbyname(socket.gethostname())
     }
   }
   return initial_info
 
+######################################################################################################
+
+# RECURRING SPECS
+def cpu_spec():
+  return psutil.cpu_percent(interval=0.1, percpu=True)
+
 def memory_spec():
   return psutil.virtual_memory().percent
 
 def disk_spec():
   return (shutil.disk_usage('/').used / shutil.disk_usage('/').total)*100
+
+def process_spec():
+  processes_info = {
+    "my_cpu_percent": cpu_percent(MY_PID),
+    "processes": processes_info_getter()
+  }
+  return processes_info
+
+def net_spec():
+  net_info = {
+    "usage_per_interface": usage_per_interface(),
+    "usage_per_process": usage_per_process()
+  }
+  return net_info
 
 def summary_spec():
   summary_info = {
@@ -49,6 +242,148 @@ def summary_spec():
   }
   return summary_info
 
+######################################################################################################
+
+# SCHEDULE
+TIME_FORMAT = '%H:%M:%S'
+start_time = call_time = exec_time = start_clock = exec_clock = 0
+
+def string_of_now():
+  return time.strftime(TIME_FORMAT, time.localtime())
+
+def get_files_by_dir(dir):
+  global call_time
+  call_time = string_of_now()
+
+  time.sleep(1)
+  files = os.listdir(dir)
+
+  global exec_time
+  exec_time = string_of_now()
+
+  global exec_clock
+  exec_clock = time.process_time()
+
+  return files
+
+def schedule_finder():
+  scheduler = sched.scheduler(time.time, time.sleep)
+
+  global start_time
+  start_time = string_of_now()
+
+  global start_clock
+  start_clock = time.process_time()
+
+  scheduler.enter(1, 3, get_files_by_dir, (DIR,))
+  scheduler.run()
+
+def schedule_spec():
+  schedule_finder()
+
+  exec_diff_time = (datetime.strptime(exec_time, TIME_FORMAT) - datetime.strptime(call_time, TIME_FORMAT)).seconds
+  clock_diff_time = round(exec_clock - start_clock, 7)
+
+  schedule_info = {
+    "start_time": start_time,
+    "call_time": call_time,
+    "exec_time": exec_time,
+    "exec_diff_time": exec_diff_time,
+    "clock_diff_time": clock_diff_time
+  }
+  return schedule_info
+
+######################################################################################################
+
+# SUBNET
+NM = PortScanner()
+
+def subnet_spec(arg):
+  hosts = get_hosts(arg)
+  hosts_info = fill_hosts_info(hosts)
+
+  subnet_info = {
+    "len": len(hosts),
+    "hosts": hosts_info
+  }
+  return subnet_info
+
+def get_hosts(arg):
+  list_ip = arg.split(".")
+  net = ".".join(list_ip[0:3]) + "."
+  hosts = verify_hosts(net)
+  return hosts
+
+def ping_code(hostname):
+  if platform.system() == "Windows":
+    args = ["ping", "-n", "2", "-l", "1", "-w", "100", hostname]
+  else:
+    args = ['ping', '-c', '1', '-W', '1', hostname]
+  ret_cod = subprocess.call(args, stdout=open(os.devnull, 'w'), stderr=open(os.devnull, 'w'))
+  return ret_cod
+
+def verify_hosts(base_ip):
+  valid_hosts = []
+  for i in range(80, 83): ##################### CHANGE TO (1, 255) BE COMPLETE
+    print('Investigando host:', i)
+    if (ping_code(base_ip + str(i)) == 0):
+      valid_hosts.append(base_ip + str(i))
+  return valid_hosts
+
+def get_formated_ip(host):
+  name = ''
+  try:
+    name = NM[host].hostname()
+  except:
+    print("Erro de hostname:", host)
+
+  if not name:
+    name = "Não possui nome disponível"
+  return(f"{host} ({name})")
+
+def get_formated_protocols(host):
+  protocols = ''
+  try:
+    protocols = NM[host].all_protocols()
+  except:
+    print("Erro de protocols:", host)
+
+  protocols = ', '.join(protocols)
+  if not protocols:
+    protocols = "Não possui protocolo disponível"
+  return(f"Protocolos: {protocols}")
+
+def get_formated_ports(host):
+  ports_infos = []
+  for proto in NM[host].all_protocols():
+    ports_infos.append(f'Protocolo : {proto}\t')
+    this_proto_ports_info = []
+    for port in NM[host][proto].keys():
+      port_info = NM[host][proto][port]
+      text_port_info = f'Porta: {port}, Nome: {port_info["name"]}, Estado: {port_info["state"]}'
+      this_proto_ports_info.append(text_port_info)
+    ports_infos.append(" \n ".join(this_proto_ports_info))
+
+  formated_ports_infos = " \n ".join(ports_infos)
+  return(formated_ports_infos)
+
+def fill_hosts_info(hosts):
+  hosts_info = []
+  for host in hosts :
+    NM.scan(hosts = host, arguments='-n -PE -PA21,23,80,3389')
+    print("Passei pelo nm", NM[host])
+    host_info = {
+      "ip": get_formated_ip(host),
+      "state": NM[host]['status']['state'],
+      "protocols": get_formated_protocols(host) or '',
+      "ports": get_formated_ports(host)
+    }
+    hosts_info.append(host_info)
+  return hosts_info
+
+######################################################################################################
+
+# MAIN
 tcp.listen()
 print('Servidor pronto!')
 (cliente,addr) = tcp.accept()
@@ -61,24 +396,26 @@ while True:
     break
   if msg == "initial":
     encoded_resp(initial_spec(), cliente)
-  # if msg == "net":
-  #   encoded_resp(net_spec(), cliente)
-  # if msg == "ip": # split with space
-  #   encoded_resp(ip_spec(args), cliente)
-  if msg == "summary":
-    encoded_resp(summary_spec(), cliente)
-  # if msg == "cpu":
-  #   encoded_resp(cpu_spec(), cliente)
+  if msg == "cpu":
+    encoded_resp(cpu_spec(), cliente)
   if msg == "memory":
     encoded_resp(memory_spec(), cliente)
   if msg == "disk":
     encoded_resp(disk_spec(), cliente)
-  # if msg == "files":
-  #   encoded_resp(files_spec(), cliente)
-  # if msg == "process":
-  #   encoded_resp(process_spec(), cliente)
+  if msg == "process":
+    encoded_resp(process_spec(), cliente)
+  if msg == "net":
+    encoded_resp(net_spec(), cliente)
+  if msg == "summary":
+    encoded_resp(summary_spec(), cliente)
+  
+  if msg == "schedule":
+    encoded_resp(schedule_spec(), cliente)
+  if msg.split('?')[0] == "subnet":
+    arg = msg.split('?')[1]
+    encoded_resp(subnet_spec(arg), cliente)
+  
   else:
     pass
-    # encoded_resp("Erro: requisicao invalida", cliente)
 
 tcp.close()
